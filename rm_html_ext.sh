7ea035dbebc8b4757572690ba340449d08a6e6ca
Piotr Patrzyk <piotr@patrzyk.me>
2020-11-20 16:06:03 +0100

    script rm

--- rm_html_ext.sh
+++
@@ -1,2 +0,0 @@
-# https://davidjuan.com/technology/2020/05/30/jekyll-s3-static-site-blog
-find _site/ -type f ! -iname 'index.html' -iname '*.html' -print0 | while read -d $'\0' f; do mv "$f" "${f%.html}"; done
